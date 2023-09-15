a1e6e4cb223ef2c303232fd554aa88aebbda41df
noara <tutoravacefet@gmail.com>
2023-09-15 03:59:19 -0300

    adding base project and gitignore

---
+++ projetos-inovadores/src/main/java/com/example/projetosinovadores/ProjetosInovadoresApplication.java
@@ -0,0 +1,13 @@
+package com.example.projetosinovadores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetosInovadoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetosInovadoresApplication.class, args);
+	}
+
+}
